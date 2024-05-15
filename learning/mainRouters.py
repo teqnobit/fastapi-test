82d17922d6f95cbfdba3659f97f6cdee714171af
root <root@ip-172-31-30-219.us-west-2.compute.internal>
2024-05-15 08:16:23 +0000

    solicitudes CORS

--- learning/mainRouters.py
+++ learning/mainRouters.py
@@ -2,12 +2,22 @@ from fastapi import FastAPI
 from routers import items, users_db
 import authenticationJWT
 from routers import users
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(items.router)
 app.include_router(users.router)
 app.include_router(authenticationJWT.router)
 app.include_router(users_db.router)
+
+# Configurar el middleware CORS para permitir todas las solicitudes desde cualquier origen
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Esto permitirá solicitudes desde cualquier origen
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def mainApi():
